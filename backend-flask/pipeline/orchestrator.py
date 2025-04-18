3d5ab310bb6f6068895f289155b6e0d0d36146e2
ognjenmedic <ognjen.medic@gmail.com>
2025-04-18 18:30:21 +0200

    updated logging in orchestrator.py

--- backend-flask/pipeline/orchestrator.py
+++ backend-flask/pipeline/orchestrator.py
@@ -5,7 +5,6 @@ logging.basicConfig(
     level=logging.INFO, 
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("app.log"),
         logging.StreamHandler()
     ]
 )
